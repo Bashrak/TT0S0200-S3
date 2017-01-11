2819bd5b3693944b28a41b6c1bff3ef30f6dc7dc
K8321 <K8321@ICT-421-WS-22>
2017-01-11 15:30:26 +0200

    T1 done, Lotto in progress

---
+++ T_01/Lotto/Lotto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Lotto
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] table = new int[40];
+            int[] winNumbers = new int[7];
+            int lotteryNumbers = rnd.Next(1,40);
+            int lines;
+
+            for (int j = 0; j < table.Length; j++)
+            {
+                table j = winNumbers;
+            }
+            
+
+            Console.WriteLine("How many lines would you like? :");
+            lines = System.Convert.ToInt32(System.Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                
+            }
+
+
+
+
+        }
+    }
+}
