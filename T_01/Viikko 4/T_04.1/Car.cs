37514947daae14bce2381652f4ca79c83ae813a3
K8321 <K8321@ICT-421-WS-22>
2017-01-25 11:59:00 +0200

    Labra 1 T1 done

---
+++ T_01/Viikko 4/T_04.1/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_04._1
+{
+    class Car
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public int tiresCount { get; }
+        public int maxTires { get; set;}
+        public int addTire { get; set; }
+
+        private List<Tire>
+    }
+}
---
+++ T_01/Viikko 4/T_04.1/Tire.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_04._1
+{
+    class Tire
+    {
+        public string manu;
+        public string tireModel;
+        public string tireSize;
+    }
+}
