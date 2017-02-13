08e269933bd8eb70635e4636aa53f9023cd04596
Bashrak <lassioja@gmail.com>
2017-02-13 12:14:49 +0200

    Add files via upload

---
+++ Labra_3/T_01/T_05/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_05
+{
+    class Student
+    {
+        string Name { get; set; }
+        string ID { get; set; }
+        int Age { get; set; }
+
+        public Student(string name, string id, int age)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+       
+        public string ToString()
+        {
+            return String.Format("Students name: {0} ID: {1} Age: {2}", Name, ID, Age);
+        }
+    }
+}
