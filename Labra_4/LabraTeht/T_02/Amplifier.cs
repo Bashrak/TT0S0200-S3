877354eae48867ea56f759cb7138964ea63f361f
K8321 <K8321@ICT-421-WS-22>
2017-02-13 12:44:56 +0200

    T_01 and T_02 done

---
+++ Labra_4/LabraTeht/T_02/Amplifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_02
+{
+    class Amplifier
+    {
+        int vol;
+        public Amplifier() { }
+        public int Vol
+        {
+            get
+            {
+                return vol;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    vol = value;
+                    Console.WriteLine("Amplifier volume is set to: " + value);
+                }
+                else
+                {
+                    Console.WriteLine("Too low volume - Amplifier volume is set to minimum");
+                    {
+                        vol = 0;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+    
---
+++ Labra_4/LabraTeht/T_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_02
+{
+    class Program
+    {
+        static void Vahvari()
+        {
+            Amplifier amp = new Amplifier();
+            Console.WriteLine("Give a value to set the amplifier volume: ");
+            amp.Vol = int.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            Vahvari();
+
+        }
+    }
+}
