a377be8d9bc6a8e5e6a708cb83576de71113d56c
K8321 <K8321@ICT-421-WS-22>
2017-02-13 13:41:33 +0200

    Started T_04

---
+++ Labra_4/LabraTeht/T_04/Bike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_04
+{
+    class Bike
+    {
+        string Name { get; set; }
+        string Model { get; set; }
+        int Year { get; set; }
+        string Gears { get; set; }
+        string GearsManu { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", "
+
+    }
+}
---
+++ Labra_4/LabraTeht/T_04/Boat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_04
+{
+    class Boat
+    {
+        
+        string Type { get; set; }
+        int SeatCount { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Seat Count: " + SeatCount + ", Boat Type: " + Type;
+        }        
+    }
+}
