4ee18ed0d5ebb73ec802f55e515709f149289bc4
Lassi Oja <k8321@student.jamk.fi>
2017-02-13 16:58:13 +0200

    T_05 Done

---
+++ Labra_4/LabraTeht/T_05/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_05
+{
+    class Program
+    {
+        static void Radio()
+        {
+            string apu;
+            Radio radio = new Radio(true);
+
+            while (true)
+            {
+                while (true)
+                {
+                    Console.Write("\nGive a frequency that you would like to listen: (2000-26000): ");
+                    radio.Frequency = float.Parse(Console.ReadLine());
+                    if (radio.Frequency > 26000 || radio.Frequency < 2000)
+                    {
+                        Console.WriteLine("Frequency is out of range, please try again");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                while (true)
+                {
+                    Console.Write("Set the volume(0-9): ");
+                    radio.Volume = int.Parse(Console.ReadLine());
+                    if (radio.Volume < 0)
+                    {
+                        Console.WriteLine("Volume is too low, please try again.");
+                    }
+                    else if (radio.Volume > 9)
+                    {
+                        Console.WriteLine("Volume is too high, please try again.");
+                    }
+                    else
+                        break;
+                }
+
+                Console.WriteLine(radio.ToString());
+                Console.WriteLine("\nListening to music.....");
+                System.Threading.Thread.Sleep(10000);
+                Console.Write("\nTo exit type quit: ");
+                apu = Console.ReadLine();
+                if (apu == "quit")
+                {
+                    radio.OnOff = false;
+                    Console.WriteLine(radio.ToString());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enjoy listening longer.");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Radio();
+        }
+    }
+}
---
+++ Labra_4/LabraTeht/T_05/Radio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_05
+{
+    class Radio
+    {
+        public bool OnOff { get; set; }
+        public int Volume { get; set; }
+        public float Frequency { get; set; }
+
+        public Radio(bool onoff)
+        {
+            OnOff = onoff;
+        }
+
+        public override string ToString()
+        {
+            if (!OnOff)
+            {
+                return "Radio is off so you can't change the volume nor the frequency";
+            }
+            else
+            {
+                return "Radio is on, set the Frequency " + Frequency + " and set the the Volume" + Volume;
+            }
+        }
+    }
+}
+
